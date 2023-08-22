15cdbd48e8c572b048df7906cf0acf476cdf699f
jacky2522561 <72062593+jacky2522561@users.noreply.github.com>
2023-08-22 11:10:28 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import Header from "./common/header/index";
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { HashRouter,Route,Routes } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from "./store/index";
 import Home from './pages/home';
@@ -11,12 +11,12 @@ class App extends Component {
       <Provider store={store}>
         <Fragment>
           <Header />
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path='/' Component={Home}></Route>
               <Route path='/detail' Component={Detail}></Route>
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </Fragment>
       </Provider>
     )
